fb7388767b2874ac5b41a8ea84926a373a75c444
Noah Krell <noahkrell14@gmail.com>
2018-02-02 15:19:08 -0600

    basic job list controller functionality, incorporated into view

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'rails', '~> 5.1.4'
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/joblist_controller.rb
+++ app/controllers/joblist_controller.rb
@@ -1,8 +1,26 @@
+require 'open-uri'
+require 'openssl'
+
 class JoblistController < ApplicationController
 
   def index
+    @city = params[:city]
+    #nokogiri stuff
+    url = "https://www.builtin#{@city}.com/jobs?f[0]=job-category_107&f[1]=job-category_324&f[2]=experience_level_1&f[3]=experience_level_1-3&f[4]=experience_level_3-5"
+    doc = Nokogiri::HTML(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+
+    #collecting each job post
+    @posts = []
+    doc.css(' div.original > div.center-left').each do |node|
+      @posts << {
+        "title" => node.children.css('.title').text,
+        "company" => node.children.css('.company-title').text,
+        "description" => node.children.css('.description').text,
+        "link" => "https://www.builtin#{@city}.com" + node.children.css('.wrap-view-page').children.attr('href').value,
+        "post-time" => node.next_element.children.css('.job-date').text
+        }
+    end
     render :index
-    binding.pry
   end
 
 end
