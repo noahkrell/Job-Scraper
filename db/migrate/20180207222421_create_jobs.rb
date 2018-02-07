a9595d473d04db66920ba7e9e2eae564c2d0a1d1
Noah Krell <noahkrell14@gmail.com>
2018-02-07 16:53:40 -0600

    seed db with nokogiri scrape, now have job objects

--- db/migrate/20180207222421_create_jobs.rb
+++ db/migrate/20180207222421_create_jobs.rb
@@ -3,11 +3,10 @@ class CreateJobs < ActiveRecord::Migration[5.1]
     create_table :jobs do |t|
       t.string :title
       t.string :company
+      t.string :location
       t.string :description
       t.string :link, unique: true
       t.string :post_time
-      t.boolean :saved, default: false
-      t.boolean :hidden, default: false
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'openssl'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+cities = ["Austin", "Boston", "Colorado", "LA", "NYC"]
+cities.each do |city| 
+  url = "https://www.builtin" + city + ".com/jobs?f[0]=job-category_107&f[1]=job-category_324&f[2]=experience_level_1&f[3]=experience_level_1-3&f[4]=experience_level_3-5"
+  doc = Nokogiri::HTML(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+
+  doc.css(' div.original > div.center-left').each do |node|
+    Job.create(title: node.children.css('.title').text,
+                company: node.children.css('.company-title').text,
+                location: city,
+                description: node.children.css('.description').text,
+                link: "https://www.builtin" + city + node.children.css('.wrap-view-page').children.attr('href').value,
+                post_time: node.next_element.children.css('.job-date').text
+                )
+  end
+end
+
+
+
+
+
+
