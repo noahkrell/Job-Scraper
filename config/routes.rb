a37e1bdeac760cdc4c8ff953d5a7d9267f5aa8e6
Noah Krell <noahkrell14@gmail.com>
2018-02-07 13:55:57 -0600

    redesign nav-bar, link up login page + profile page, finish sessions controller and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
   
   resources :users
 
