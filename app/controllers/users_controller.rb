43c9a950d3e690193ab6f361d770aceca8fd2495
Noah Krell <noahkrell14@gmail.com>
2018-02-06 21:47:50 -0600

    Complete user signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the JobScraper! Account successfully created."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :first_name, :last_name, :email, :password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homepage#index'
   get '/jobs' => 'joblist#index'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 
 end
