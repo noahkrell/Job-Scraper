d6fdc00e7b8d5fed271a7514575071af85c682d2
Noah Krell <noahkrell14@gmail.com>
2018-02-07 12:18:28 -0600

    add sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get '/jobs' => 'joblist#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+  
   resources :users
 
 end
