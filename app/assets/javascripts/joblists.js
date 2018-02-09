763502bffff01f25f4aa1e6021ce74496dee6b24
Noah Krell <noahkrell14@gmail.com>
2018-02-08 20:36:52 -0600

    add alert on save/hide buttons until db functionality is configured

---
+++ app/assets/javascripts/joblists.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  console.log('hello')
+
+  $('.save-button').on('click', function(e) {
+    e.preventDefault()
+    alert('function not yet available... check back soon!')
+  });
+
+  $('.hide-button').on('click', function(e) {
+    e.preventDefault()
+    alert('function not yet available... check back soon!')
+  });
+
+});
+
