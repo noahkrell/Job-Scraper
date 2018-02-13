ab3f2cc9701905edb28f0dd0eec0346720879e6a
Noah Krell <noahkrell14@gmail.com>
2018-02-13 16:02:34 -0600

    order featured jobs by updated_at time, and only display 3 most recently updated

--- app/controllers/joblist_controller.rb
+++ app/controllers/joblist_controller.rb
@@ -5,9 +5,15 @@ class JoblistController < ApplicationController
 
   def index
     @city = params[:city]
-    jobs = Job.where(location: @city)
-    # sort the job posts by age (see: class Job age method)
+    
+    # sort featured job posts by when they were updated last
+    featured_jobs = Job.where(location: @city, post_time: "")
+    @featured_posts = featured_jobs.order(updated_at: :desc)
+    
+    # sort regular job posts by age (see: class Job age method)
+    jobs = Job.where(location: "Austin").where.not(post_time: "")
     @posts = jobs.sort_by { |obj| obj.age }
+
     render :index
   end
 end
