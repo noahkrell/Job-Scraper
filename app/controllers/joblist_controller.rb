d51d44ec7b55550d7f90c5681b3a872c23c07f78
Noah Krell <noahkrell14@gmail.com>
2018-02-13 16:09:25 -0600

    fix location bug on joblist pages

--- app/controllers/joblist_controller.rb
+++ app/controllers/joblist_controller.rb
@@ -5,13 +5,13 @@ class JoblistController < ApplicationController
 
   def index
     @city = params[:city]
-    
+
     # sort featured job posts by when they were updated last
     featured_jobs = Job.where(location: @city, post_time: "")
     @featured_posts = featured_jobs.order(updated_at: :desc)
     
     # sort regular job posts by age (see: class Job age method)
-    jobs = Job.where(location: "Austin").where.not(post_time: "")
+    jobs = Job.where(location: @city).where.not(post_time: "")
     @posts = jobs.sort_by { |obj| obj.age }
 
     render :index
