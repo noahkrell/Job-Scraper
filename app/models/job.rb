5a1052e0c733e19f814fc47eaa3c1c932fcd501e
Noah Krell <noahkrell14@gmail.com>
2018-02-14 12:38:08 -0600

    refactor age method slightly to account for edge cases

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,24 +3,29 @@ class Job < ApplicationRecord
   acts_as_votable
 
   def age
-    if self.post_time.empty?
+    time = self.post_time
+
+    if time.empty?
       return 0
-    elsif self.post_time.include?("hour")
+    elsif time.include?("hour") || time.include?("minutes")
       return 1
-    elsif self.post_time.include?("1 day")
+    elsif time.include?("1 day")
       return 2
-    elsif self.post_time.include?("2 days")
+    elsif time.include?("2 days")
       return 3
-    elsif self.post_time.include?("3 days")
+    elsif time.include?("3 days")
       return 4
-    elsif self.post_time.include?("4 days")
+    elsif time.include?("4 days")
       return 5
-    elsif self.post_time.include?("5 days")
+    elsif time.include?("5 days")
       return 6
-    elsif self.post_time.include?("6 days")
+    elsif time.include?("6 days")
       return 7
-    else
+    elsif time.include?("1 week")
       return 8
+    elsif time.include?("2 weeks")
+      return 9
     end
   end
+
 end
